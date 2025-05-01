8f73e5468ff959631d04cd3e6f8e94dce2f2ee3f
praveen choudhary <pc852180@gmail.com>
2025-05-01 11:40:09 +0530

    Task-Managment-System

--- client/src/Admin/Adminlogin.jsx
+++ client/src/Admin/Adminlogin.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 function Adminlogin() {
+   
+  const [input, setInput] = useState({});
+
+
+
+
   return (
     <>
     <div id="from">
@@ -9,18 +15,18 @@ function Adminlogin() {
      <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Enter name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" />
+        <Form.Control type="text" placeholder="Enter name" name='name' value={input.name} onChange={handelInput} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" name='email' value={input.name} onChange={handelInput} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" name='password' value={input.password} onChange={handelInput} />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={handelSubmit}>
         Submit
       </Button>
     </Form>
