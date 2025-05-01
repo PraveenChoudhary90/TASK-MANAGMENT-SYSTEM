df0a3deed188cc1ea74e01c8ce2a997aac87341b
praveen choudhary <pc852180@gmail.com>
2025-05-01 11:36:38 +0530

    Task-Managment-System

--- client/src/Admin/Adminlogin.jsx
+++ client/src/Admin/Adminlogin.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 function Adminlogin() {
   return (
     <>
-    <h1>Admin Login page</h1>
+    <div id="from">
+    <h1>Admin Login</h1>
+     <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Enter name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
     </>
   )
 }
--- client/src/User/Userlogin.jsx
+++ client/src/User/Userlogin.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 function Userlogin() {
   return (
     <>
-    <h1>User Login Page</h1>
+    <div id="from">
+    <h1>User Login</h1>
+     <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
     </>
   )
 }
